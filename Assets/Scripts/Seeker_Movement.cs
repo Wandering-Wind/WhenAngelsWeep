086f8b2762a68a7d2a055a5e936ac6db87d3376e
benete21 <benyubun@gmail.com>
2026-04-05 23:22:00 +0200

    addiing seeker movmet

---
+++ Assets/Scripts/Seeker_Movement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Seeker_Movement : NetworkBehaviour
+{
+
+    [Header("Player Components")]
+    [SerializeField] private Transform cameraPivot;
+    [SerializeField] private Camera playerCamera;
+
+    [Header("Player Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float lookSensitivity = 2f;
+    [SerializeField] private float maxPitch = 80f;
+
+    private PlayerInput pi;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+    private InputAction sneakAction;
+    private CharacterController cc;
+
+    private float pitch;
+
+    public override void OnNetworkSpawn()
+    {
+        cc = GetComponent<CharacterController>();
+        pi = GetComponent<PlayerInput>();
+
+        if (!IsOwner)
+        {
+            if (playerCamera) playerCamera.enabled = false;
+            if (pi) pi.enabled = false;
+            enabled = false;
+            return;
+        }
+
+        moveAction = pi.actions["Move"];
+        lookAction = pi.actions["Look"];
+        jumpAction = pi.actions["Jump"];
+        sneakAction = pi.actions["Sneak"];
+        moveAction.Enable();
+        lookAction.Enable();
+        jumpAction.Enable();
+        sneakAction.Enable();
+
+        if (playerCamera) playerCamera.enabled = true;
+    }
+
+    private void Update()
+    {
+        Vector2 m = moveAction.ReadValue<Vector2>();
+        Vector3 move = transform.right * m.x + transform.forward * m.y;
+        cc.Move(move * moveSpeed * Time.deltaTime);
+
+        Vector2 look = lookAction.ReadValue<Vector2>() * lookSensitivity;
+        transform.Rotate(0f, look.x, 0f);
+
+        pitch -= look.y;
+        pitch = Mathf.Clamp(pitch, -maxPitch, maxPitch);
+        cameraPivot.localEulerAngles = new Vector3(pitch, 0f, 0f);
+    }
+}
+
