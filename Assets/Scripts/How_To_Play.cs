a33fa007ee8ab0d8ff191f439d4f2a8af6beb95b
benete21 <benyubun@gmail.com>
2026-04-27 17:53:42 +0200

    finish adding how to play

--- Assets/Scripts/How_To_Play.cs
+++ Assets/Scripts/How_To_Play.cs
@@ -3,12 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class How_To_Play : MonoBehaviour
 {
-    public class SimpleSceneLoader : MonoBehaviour
-    {
         [SerializeField] private string sceneName;
         public void LoadScene()
         {
             SceneManager.LoadScene(sceneName);
         }
-    }
 }
